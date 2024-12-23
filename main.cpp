902dbb6bed205dd1e7ae98ebd55940f321fb0928
Keith Ohanlon <keith@Keiths-MacBook-Air.local>
2024-12-23 22:50:02 +0000

    formatting

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ const TGAColor red = TGAColor(255, 0, 0, 255);
 int main(int argc, char **argv) {
   TGAImage image(100, 100, TGAImage::RGB);
   image.set(52, 41, red);
-  image.flip_vertically(); // i want to have the origin at the left bottom
-                           // corner of the image
+  // i want to have the origin at the left bottom  corner of the image
+  image.flip_vertically();
   image.write_tga_file("output.tga");
   return 0;
 }
