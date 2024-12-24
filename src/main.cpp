05ad0979ada363a19ec29f090f0306043df09220
Keith Ohanlon <keith@Keiths-MacBook-Air.local>
2024-12-24 14:56:08 +0000

    parsing + rendering obj files

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,40 @@
 #include "line.h"
+#include "model.h"
 #include "tgaimage.h"
+#include <iostream>
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 
+const char *objPath = "assets/african_head.obj";
+
+Model *model = NULL;
+
+const int width = 800;
+const int height = 800;
+
 int main(int argc, char **argv) {
-  TGAImage image(100, 100, TGAImage::RGB);
+  TGAImage image(width, height, TGAImage::RGB);
 
-  lineFast(13, 20, 80, 40, image, white);
-  lineFast(20, 13, 40, 80, image, red);
-  lineFast(80, 40, 13, 20, image, red);
+  model = new Model(objPath);
+  for (int i = 0; i < model->nfaces(); i++) {
+    std::vector<int> face = model->face(i);
+    for (int j = 0; j < 3; j++) {
+      Vec3f v0 = model->vert(face[j]);
+      Vec3f v1 = model->vert(face[(j + 1) % 3]);
 
-  // i want to have the origin at the left bottom  corner of the image
+      int x0 = (v0.x + 1.) * width / 2.;
+      int y0 = (v0.y + 1.) * height / 2.;
+      int x1 = (v1.x + 1.) * width / 2.;
+      int y1 = (v1.y + 1.) * height / 2.;
+
+      lineFast(x0, y0, x1, y1, image, white);
+    }
+  }
+
+  // i want to have the origin at the left bottom corner of the image
   image.flip_vertically();
   image.write_tga_file("output.tga");
+
   return 0;
 }
